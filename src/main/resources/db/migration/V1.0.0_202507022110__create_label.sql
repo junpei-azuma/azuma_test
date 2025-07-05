939df9c76bf82bf78a1c580e8ba92caf065ace8a
junpei.azuma <azumanotdetail@gmail.com>
2025-07-05 14:41:53 +0900

    feat: PostgreSQL用のデータソース設定とFlywayの設定を追加

---
+++ src/main/resources/db/migration/V1.0.0_202507022110__create_label.sql
@@ -0,0 +1,8 @@
+CREATE TABLE label (
+    id VARCHAR(26) NOT NULL, -- UUID形式のID. アプリケーション側で生成する
+    name VARCHAR(255) NOT NULL,
+    color VARCHAR(20) DEFAULT '#FFFFFF', -- ラベルの色を表す16進数カラーコード
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
---
+++ src/main/resources/db/migration/V1.0.0_202507022120__create_task_label.sql
@@ -0,0 +1,7 @@
+CREATE TABLE task_label (
+    task_id VARCHAR(26) NOT NULL,
+    label_id VARCHAR(26) NOT NULL,
+    PRIMARY KEY (task_id, label_id),
+    FOREIGN KEY (task_id) REFERENCES task(id) ON DELETE CASCADE,
+    FOREIGN KEY (label_id) REFERENCES label(id) ON DELETE CASCADE
+);

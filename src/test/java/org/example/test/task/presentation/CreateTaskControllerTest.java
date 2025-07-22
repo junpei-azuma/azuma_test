12f688e66d0a83ba6b37882481aeeda2fb367324
junpei.azuma <azumanotdetail@gmail.com>
2025-07-22 17:05:10 +0900

    test: タスク作成機能の正常系テストを追加

--- src/test/java/org/example/test/task/presentation/CreateTaskControllerTest.java
+++ src/test/java/org/example/test/task/presentation/CreateTaskControllerTest.java
@@ -11,8 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.time.LocalDate;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,18 +21,46 @@ public class CreateTaskControllerTest {
   private MockMvc mockMvc;
 
   @Test
-  @DisplayName("入力値エラーのテスト")
-  void testCreateTaskWithInvalidInput() throws Exception {
+  @DisplayName("タスク作成の正常系テスト")
+  void testCreateTask() throws Exception {
     var startDate = LocalDate.now().plusDays(1);
     var dueDate = LocalDate.now().plusDays(30);
     var requestBody = """
         {
-          "title": "",
+          "title": "Test Task",
           "completeCondition": "test condition",
           "startDate": "%s",
           "dueDate": "%s"
         }
         """.formatted(startDate, dueDate);
+    mockMvc.perform(post("/tasks")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(requestBody))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.id").isString())
+        .andExpect(jsonPath("$.title").value("Test Task"))
+        .andExpect(jsonPath("$.completeCondition").value("test condition"))
+        .andExpect(jsonPath("$.startDate").value(startDate.toString()))
+        .andExpect(jsonPath("$.dueDate").value(dueDate.toString()))
+        .andExpect(jsonPath("$.status").value("NOT_STARTED"))
+        .andExpect(jsonPath("$.isPostponed").value(false));
+  }
+
+  @Test
+  @DisplayName("入力値エラーのテスト")
+  void testCreateTaskWithInvalidInput() throws Exception {
+    var startDate = LocalDate.now().plusDays(1);
+    var dueDate = LocalDate.now().plusDays(30);
+    var inValidCompleteCondition = "a".repeat(501);
+    var requestBody = """
+        {
+          "title": "",
+          "completeCondition": "%s",
+          "startDate": "%s",
+          "dueDate": "%s"
+        }
+        """.formatted(inValidCompleteCondition, startDate, dueDate);
     mockMvc.perform(post("/tasks")
             .contentType(MediaType.APPLICATION_JSON)
             .content(requestBody))
